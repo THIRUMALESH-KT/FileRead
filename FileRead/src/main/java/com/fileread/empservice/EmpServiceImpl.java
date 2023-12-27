6fd8e90f6c53d88718081da61a2e5b881d0f76c6
THIRUMALESH-KT <tthirumal24@gmail.com>
2023-12-27 09:12:11 +0530

    update3

--- FileRead/src/main/java/com/fileread/empservice/EmpServiceImpl.java
+++ FileRead/src/main/java/com/fileread/empservice/EmpServiceImpl.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,25 +33,43 @@ public class EmpServiceImpl implements IEmpService {
 		return processFile(file);
 		
 	}
-	private List<Employee> processFile(String file) {
-		log.info("inside processFile service");
-		String line="";
-		try {
-			BufferedReader br=new BufferedReader(new FileReader(file));
-			
-			while((line=br.readLine())!=null) {
-				String[] data=line.split("[, \\|]");
-				
-				Employee emp=new Employee(Long.valueOf(data[0]), data[1],Double.valueOf(data[2]), data[3]);
+private List<Employee> processFile(String file) {
+String line="";
+	try {
+		BufferedReader br=new BufferedReader(new FileReader(file));
+		while((line=br.readLine())!=null) {
+			StringTokenizer st=new StringTokenizer(line, ("[, \\|]"));
+			while(st.hasMoreTokens()) {
+				Employee emp=new Employee(Long.valueOf(st.nextToken()),st.nextToken(),Double.valueOf(st.nextToken()),st.nextToken());
 				empRepo.save(emp);
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		return empRepo.findAll();
-		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
+	
+	return empRepo.findAll();
+	}
+//	private List<Employee> processFile(String file) {
+//		log.info("inside processFile service");
+//		String line="";
+//		try {
+//			BufferedReader br=new BufferedReader(new FileReader(file));
+//			
+//			while((line=br.readLine())!=null) {
+//				String[] data=line.split("[, \\|]");
+//				
+//				Employee emp=new Employee(Long.valueOf(data[0]), data[1],Double.valueOf(data[2]), data[3]);
+//				empRepo.save(emp);
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return empRepo.findAll();
+//		
+//	}
 	@Override
 	public List<Employee> readData() {
 		log.info("inside readData servcie");
